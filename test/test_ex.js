9bbdc8409d2566d15a223b00329050d874febfe9
Kim eunji <emliy112@naver.com>
2021-03-27 19:20:53 +0900

    js array 응용

--- test/test_ex.js
+++ test/test_ex.js
@@ -23,4 +23,65 @@ books.splice(2,0,book)
 //console.log(books)
 //
 books.splice(2,1) // ??
-console.log(books)
+//console.log(books)
+
+// title 바꾸기1
+const b = books.find(function (item) {
+    if (item.title === "Seven Dwarfs") {
+        return true
+    }
+})
+b.author = "Tom"
+
+// title 바꾸기2
+const c = books.find(function (item) {
+    if (item.author === "Unknown") {
+        return true
+    }
+})
+c.author = "eunji"
+
+
+// price > 30 인거 찾아서 10% 할인 한 걸로 바꾸기
+const p = books.find(function (item) {
+    if (item.price >30) {
+        return true
+    }
+})
+p.price = p.price * 0.9;
+
+
+/*
+for i in books {
+    if (books.price > 20) {
+        books.price = books.price * 90
+    }
+
+}
+*/
+
+//console.log(books)
+
+
+let sum = 0;
+books.forEach(item => sum =+ item.price)
+console.log(sum)
+
+
+const higeRandList = books.filter(item => item.rank > 3)
+console.log(higeRandList)
+
+
+
+//
+const rankTitle = [...books].sort((a, b) => {
+        if (a.title > b.title) {
+            return 1;
+        } else if (a.title < b.title) {
+            return -1
+        } else {
+            return 0;
+        }
+    }).map((item, index) => `${index + 1}. ${item.title}`)
+console.log(rankTitle)
+
